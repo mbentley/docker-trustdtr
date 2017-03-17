325d703b25ed5f2047eadefc74a5f26528a5169b
Matt Bentley <matt.bentley@docker.com>
2017-03-17 13:14:37 -0500

    Follow better coding guidlines
    Signed-off-by: Matt Bentley <matt.bentley@docker.com>

--- trustdtr.sh
+++ trustdtr.sh
@@ -20,8 +20,8 @@ fi
 # check to see if the DTR FQDN directory exists
 if [ ! -d "/etc/docker/certs.d/${DTR}" ]
 then
-  echo -n "Creating the dirctory '/etc/docker/certs.d/${DTR}'..."
-  mkdir -p /etc/docker/certs.d/${DTR}
+  printf "Creating the dirctory '/etc/docker/certs.d/%s'..." "${DTR}"
+  mkdir -p /etc/docker/certs.d/"${DTR}"
   echo "done"
 fi
 
@@ -33,42 +33,42 @@ else
   echo "Using the server certificate for trusting DTR"
 fi
 
-echo -n "Adding certificate to '/etc/docker/certs.d/${DTR}/ca.crt'..."
+printf "Adding certificate to '/etc/docker/certs.d/%s/ca.crt'..." "${DTR}"
 
 # check to see if we should get the root or server certificate and act accordingly
 if [ "${ROOT_CERT}" = "true" ]
 then
   # get CA cert from DTR
-  curl -ksf https://${DTR}/ca > /etc/docker/certs.d/${DTR}/ca.crt
+  curl -ksf https://"${DTR}"/ca > /etc/docker/certs.d/"${DTR}"/ca.crt
 else
   # get server certificate from DTR
-  openssl s_client -host ${DTR} -port 443 </dev/null 2>/dev/null | openssl x509 -outform PEM > /etc/docker/certs.d/${DTR}/ca.crt
+  openssl s_client -host "${DTR}" -port 443 </dev/null 2>/dev/null | openssl x509 -outform PEM > /etc/docker/certs.d/"${DTR}"/ca.crt
 fi
 
 # verify command worked to get the certificate
+# shellcheck disable=SC2181
 if [ "$?" -ne "0" ]
 then
   echo "error"
-  echo -e "\nFailed to get the certificate"
+  printf "\nFailed to get the certificate\n"
   exit 1
 else
   echo "done"
 fi
 
 # verify certificate is an actual certificate; remove if not
-echo -n "Verifying format of certificate..."
-openssl x509 -in /etc/docker/certs.d/${DTR}/ca.crt -text -noout >/tmp/openssl_output 2>&1
+printf "Verifying format of certificate..."
 
 # verify openssl verification succeeded
-if [ "$?" -ne "0" ]
+if openssl x509 -in /etc/docker/certs.d/"${DTR}"/ca.crt -text -noout >/tmp/openssl_output 2>&1
 then
-  echo -e "error\n"
-  echo "Certificate validation failed:"
-  cat /tmp/openssl_output
-  echo -e "\nCleaning up '/etc/docker/certs.d/${DTR}'"
-  rm /etc/docker/certs.d/${DTR}/ca.crt /tmp/openssl_output
-  exit 1
-else
   rm /tmp/openssl_output
   echo "done"
+else
+  printf "error\n\n"
+  echo "Certificate validation failed:"
+  cat /tmp/openssl_output
+  printf "\nCleaning up '/etc/docker/certs.d/%s'\n" "${DTR}"
+  rm /etc/docker/certs.d/"${DTR}"/ca.crt /tmp/openssl_output
+  exit 1
 fi
